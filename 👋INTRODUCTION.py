eebb0508b30275efceffd116793e1ef1372c6596
bhattathakur <bhattathaur2015@gmail.com>
2024-11-29 00:54:25 -0500

    emoji added

--- 👋INTRODUCTION.py
+++ 👋INTRODUCTION.py
@@ -21,7 +21,7 @@ st.markdown(f"<h2 style='text-align:center;color:magenta;font-weight:bold;font-f
 st.markdown("""
         <div class='custom-font'>
 1. The screener allows you to view the top gainers and losers for various timeframes: the last trading day, last week, last month, and Year-to-Date (YTD).<br>
-2. Corresponding "DOWNLOADABLE" table with all information is generated at "SCREENED-TABLE" for the selection at "SCREENER."<br>
+2. Corresponding "DOWNLOADABLE" table with all information is generated at "INFO-TABLE" for the selection at "SCREENER."<br>
 3. You can filter tickers in the screener by indices such as the S&P 500, S&P 500 Sectors, NASDAQ 100, or Dow 30, providing valuable insights for more granular market observation.<br>
 4. The SMA-N-MISC indicator helps assess the position of the last price relative to key Simple Moving Averages (SMA) — 21, 50, and 200 periods — which can indicate potential mean reversion opportunities. <br>
 5. SMA-N-MISC also includes data on the 52-week highs and lows, enhancing trend analysis. <br>
