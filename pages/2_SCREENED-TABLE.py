4dfdbb7b10c0788a8ec35ba844c721ee4735b07f
bhattathakur <bhattathaur2015@gmail.com>
2024-11-28 21:04:52 -0500

    added for debugging

--- pages/2_SCREENED-TABLE.py
+++ pages/2_SCREENED-TABLE.py
@@ -13,7 +13,7 @@ st.markdown("<h2 style='text-align:center;color:magenta'>Data Frame/Table based 
 if 'dataframe' in st.session_state:
     #st.write("Active session_state keys and values:")
     df=st.session_state['dataframe'].reset_index(drop=True) #dictionary
-    #st.write(st.session_state)
+    st.write(st.session_state)
     text_values_list=[]
     for key,value in st.session_state.items():
         if key=='dataframe':continue
