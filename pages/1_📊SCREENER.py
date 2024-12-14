ff68860ad4a2872e6af6fc71c28ee6e68da0f497
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-13 22:46:15 -0500

    Update 1_📊SCREENER.py

--- pages/1_📊SCREENER.py
+++ pages/1_📊SCREENER.py
@@ -3,7 +3,7 @@ import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 
-st.set_page_config(layout="wide")  # Enable wide mode
+#st.set_page_config(layout="wide")  # Enable wide mode
 
 
 #data files sources
