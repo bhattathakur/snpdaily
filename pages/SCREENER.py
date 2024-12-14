29b2b3545a4f39b4eee1efb4d628d5cf49358928
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-13 22:45:56 -0500

    Update SCREENER.py

--- pages/SCREENER.py
+++ pages/SCREENER.py
@@ -1,5 +1,8 @@
 #loading different files as tabs 
 import streamlit as st
+# This must be the first Streamlit command
+st.set_page_config(page_title="Multi-Tab App", layout="wide")
+
 tab_list=['SCREENER','PIE_CHART','INFO_TABLE']
 tab1,tab2,tab3=st.tabs(tab_list)
 # Load example1.py content in Tab 1
