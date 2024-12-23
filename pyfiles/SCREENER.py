97979c0cb5fe6ef45a565897cddcc1657a335883
Thakur Bhatta <bhattathakur2015@gmail.com>
2024-12-23 17:49:29 -0500

    30 tickers for gapped up or down

--- pyfiles/SCREENER.py
+++ pyfiles/SCREENER.py
@@ -220,7 +220,7 @@ if side_bar_selection in ['SNP500','SNP500-SECTOR','DOW','NASDAQ100','IPO']:
                 con_df=temp_df.query('gap>0').sort_values(by='gap',ascending=False)
                 #length of the dataframe or head 20 what ever is bigger
                 lenght_of_df=len(con_df)
-                head=20 if lenght_of_df>20 else lenght_of_df
+                head=30 if lenght_of_df>20 else lenght_of_df
                 con_df=con_df.head(head)
 
                 #inserting gap in second position
@@ -231,7 +231,7 @@ if side_bar_selection in ['SNP500','SNP500-SECTOR','DOW','NASDAQ100','IPO']:
                 con_df=temp_df.query('gap<0').sort_values(by='gap',ascending=True)
                 #length of the dataframe or head 20 what ever is bigger
                 lenght_of_df=len(con_df)
-                head=20 if lenght_of_df>20 else lenght_of_df
+                head=30 if lenght_of_df>20 else lenght_of_df
                 con_df=con_df.head(head)
                 #inserting gap in second position
                 con_df.insert(1,'opening_gap',con_df.pop('gap'))
